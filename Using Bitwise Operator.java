df771503aafe6d2cb1bce04ea813b7e989c770e0
Prachi Pawar <99901892+PrachiPawar18@users.noreply.github.com>
2022-03-28 23:59:18 +0530

    Add files via upload

---
+++ Using Bitwise Operator.java
@@ -0,0 +1,21 @@
+package co.prachi;
+
+public class Program1 {
+	public static void main(String[] args) {
+		
+int n1,n2;
+n1=65;
+System.out.println(n1<<1);
+
+short num1,num2;
+num1=64;
+num2=32;
+System.out.println(num1&num2);
+System.out.println(num1|num2);
+System.out.println(~num1);
+
+	}
+
+	}
+
+
