44ff60edbae0e1de2a8f283f23ddd7d067da9629
Prachi Pawar <99901892+PrachiPawar18@users.noreply.github.com>
2022-03-30 22:54:20 +0530

    Add files via upload

---
+++ Using Variables.java
@@ -0,0 +1,27 @@
+package co.prachi;
+
+public class Program1 {
+	public static void main(String[] args) {
+		
+		//integer
+		int num;
+		num=10;
+		System.out.println("num ="+num);
+		System.out.println("max value of byte is "+Byte.MAX_VALUE);
+		System.out.println("min value of byte is "+Byte.MIN_VALUE);
+		System.out.println("Largest value we can have for integer is  "+Long.MAX_VALUE);
+		System.out.println("Minimum value for integer is "+Long.MIN_VALUE);
+	
+		//Boolean
+		boolean tf;
+		tf=true;
+		System.out.println("tf ="+tf);
+		System.out.println("!tf = "+(!tf));
+	
+		//character
+		char ch;
+		ch=100;
+		System.out.println("ch ="+ch);
+	}
+
+}
